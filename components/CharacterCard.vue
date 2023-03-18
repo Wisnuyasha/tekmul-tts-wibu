c9e71a1f0dfbd5fc25ae5885532d015ab4892b56
treeoon <wisnuyeahfaizal@gmail.com>
2023-03-18 11:42:00 +0700

    help

--- components/CharacterCard.vue
+++ components/CharacterCard.vue
@@ -1,39 +1,9 @@
 <!-- Please remove this file from your project -->
 <template>
-    <div class="bg-white flex justify-center cursor-pointer overflow-hidden shadow p-6 m-5 rounded-lg
-    transition ease-in delay-150 duration-300 hover:items-center hover:scale-110"
-    @click="redirect"
-    >
-    <img src="~/assets/Harukacrop.png" class="z-1 inset-0"/>
-      <h2 class="text-2xl leading-7 font-semibold z-2">
-        {{  title  }}
-      </h2>
-    </div>
+
 </template>
   
   <script>
-  export default {
-    name: 'CharacterCard',
-    props: {
-        title: {
-            type: String,
-            required: true
-        }
-    },
-    methods: {
-        redirect() {
-            console.log(this.title);
-            this.$router.push({
-                name: 'character',
-                params: {
-                    id: this.title
-                },
-                query: {
-                    title: this.title
-                }
-            })
-        }
-    }
-  }
+
   </script>
   
