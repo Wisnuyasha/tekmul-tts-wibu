00482f2df3e6b42144662d31fdb875356ab3c49c
Azril <akuazril12@gmail.com>
2023-03-27 20:43:10 +0700

    fix

--- pages/_char.vue
+++ pages/_char.vue
@@ -37,8 +37,11 @@
                       <div class="h-full min-h-[8rem] flex justify-center items-center">
                           <div class="h-5 opacity-50">
                               <svg v-if="audio == null" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-music"><path d="M9 18V5l12-2v13"></path><circle cx="6" cy="18" r="3"></circle><circle cx="18" cy="16" r="3"></circle></svg>
-                              <audio v-if="audio != null" class="w-full" controls="" preload="metadata" :src="audio"></audio>
-                          </div>
+                              <audio v-if="audio != null" controls>
+                                <source :src="audio" type="audio/wav">
+                                Doesnt support
+                            </audio>
+                            </div>
                       </div>
                   </div>
               </div>
