fd59c0cb920b5ef456b6c5075123f5ae173bd369
Azril <akuazril12@gmail.com>
2023-03-27 20:17:34 +0700

    fix

--- pages/_char.vue
+++ pages/_char.vue
@@ -7,67 +7,43 @@
             <img :src="characterList[this.$route.params.char].image" class="mt-3 mx-auto border-2 rounded-lg object-cover w-[350px] h-[580px] backdrop-blur-md bg-slate-200/[.06] shadow" width="500" alt="">
         </div>
         <div class="ml-5 mt-3 mb-2 ">
-            <div class="p-2 rounded-lg border-2 backdrop-blur-md bg-slate-200/[.06] shadow">
-                <div class="px-3 py-2 border-2 rounded-lg">
-                    <label class="block w-full">
-                        <span class="text-gray-500 text-sm">Text (500 words limitation)</span>
-                        <textarea v-model="text" data-testid="textbox" class="scroll-hide w-full rounded-lg border-2 p-2" placeholder="" rows="7" style="overflow-y: scroll; height: 168px;"></textarea>
-                    </label>
-                </div>
-                <div class="relative w-full border-2 mt-2 px-3 py-2 rounded-lg">
-                    <label for="default-range" class="block mb-2 text-sm">
-                        <span class="text-gray-500 text-sm">Speed</span>
-                        <span class="text-gray-500 text-sm absolute right-3">{{speed}}</span>
-                    </label>
-                    <input id="default-range" type="range" min="0.1" max="2.0" v-model="speed" step="0.1" class="w-full h-2 bg-sky-500 rounded-lg appearance-none cursor-pointer">
-                </div>
-                <div class="w-full mt-2 rounded-lg p-2 text-center font-bold bg-sky-400/[0.7] cursor-pointer hover:bg-sky-400">
-                    <h1 @click="generateAudio" class="text-sky-900">Generate</h1>
-                </div>
-            </div>
-            <div>
-                <div class="p-2 rounded-lg border-2">
-                    <div class="px-3 py-2 border-2 rounded-lg">
-                        <label class="block w-full">
-                            <span class="text-gray-500 text-sm">Text (500 words limitation)</span>
-                            <textarea v-model="text" data-testid="textbox" class="scroll-hide w-full rounded-lg border-2 p-2" placeholder="" rows="7" style="overflow-y: scroll; height: 168px;"></textarea>
-                        </label>
-                    </div>
-                    <div class="relative w-full border-2 mt-2 px-3 py-2 rounded-lg">
-                        <label for="default-range" class="block mb-2 text-sm">
-                            <span class="text-gray-500 text-sm">Speed</span>
-                            <span class="text-gray-500 text-sm absolute right-3">{{speed}}</span>
-                        </label>
-                        <input id="default-range" type="range" min="0.1" max="2.0" v-model="speed" step="0.1" class="w-full h-2 bg-sky-500 rounded-lg appearance-none cursor-pointer">
-                    </div>
-                    <div class="w-full mt-2 rounded-lg p-2 text-center font-bold bg-sky-400/[0.7] cursor-pointer hover:bg-sky-400">
-                        <h1 @click="generateAudio" class="text-sky-900">Generate</h1>
-                    </div>
-                </div>
-                <div class="gr-block gr-box relative w-full overflow-hidden border-solid border border-gray-200 mt-2 rounded-lg">
-                    <div class="absolute left-0 top-0 py-1 px-2 rounded-br-lg shadow-sm text-xs text-gray-500 flex items-center pointer-events-none bg-white z-20 border-b border-r border-gray-100">
-                        <span class="mr-2 h-[12px] w-[12px] opacity-80">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-music"><path d="M9 18V5l12-2v13"></path><circle cx="6" cy="18" r="3"></circle><circle cx="18" cy="16" r="3"></circle></svg>
-                        </span>
-                        Output Audio
-                    </div>
-                    <div id="component-19" class="gr-block gr-box relative w-full overflow-hidden border-solid border border-gray-200 mt-2 rounded-lg">
-                        <div class="absolute left-0 top-0 py-1 px-2 rounded-br-lg shadow-sm text-xs text-gray-500 flex items-center pointer-events-none bg-white z-20 border-b border-r border-gray-100">
-                            <span class="mr-2 h-[12px] w-[12px] opacity-80">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-music"><path d="M9 18V5l12-2v13"></path><circle cx="6" cy="18" r="3"></circle><circle cx="18" cy="16" r="3"></circle></svg>
-                            </span>
-                            Output Audio
-                        </div>
-                        <div class="h-full min-h-[8rem] flex justify-center items-center">
-                            <div class="h-5 opacity-50">
-                                <svg v-if="audio == null" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-music"><path d="M9 18V5l12-2v13"></path><circle cx="6" cy="18" r="3"></circle><circle cx="18" cy="16" r="3"></circle></svg>
-                                <audio v-if="audio != null" class="w-full" controls="" preload="metadata" :src="audio"></audio>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+          <div>
+              <div class="p-2 rounded-lg border-2">
+                  <div class="px-3 py-2 border-2 rounded-lg">
+                      <label class="block w-full">
+                          <span class="text-gray-500 text-sm">Text (500 words limitation)</span>
+                          <textarea v-model="text" data-testid="textbox" class="scroll-hide w-full rounded-lg border-2 p-2" placeholder="" rows="7" style="overflow-y: scroll; height: 168px;"></textarea>
+                      </label>
+                  </div>
+                  <div class="relative w-full border-2 mt-2 px-3 py-2 rounded-lg">
+                      <label for="default-range" class="block mb-2 text-sm">
+                          <span class="text-gray-500 text-sm">Speed</span>
+                          <span class="text-gray-500 text-sm absolute right-3">{{speed}}</span>
+                      </label>
+                      <input id="default-range" type="range" min="0.1" max="2.0" v-model="speed" step="0.1" class="w-full h-2 bg-sky-500 rounded-lg appearance-none cursor-pointer">
+                  </div>
+                  <div class="w-full mt-2 rounded-lg p-2 text-center font-bold bg-sky-400/[0.7] cursor-pointer hover:bg-sky-400">
+                      <h1 @click="generateAudio" class="text-sky-900">Generate</h1>
+                  </div>
+              </div>
+              <div class="gr-block gr-box relative w-full overflow-hidden border-solid border border-gray-200 mt-2 rounded-lg">
+                  <div id="component-19" class="gr-block gr-box relative w-full overflow-hidden border-solid border border-gray-200 rounded-lg">
+                      <div class="absolute left-0 top-0 py-1 px-2 rounded-br-lg shadow-sm text-xs text-gray-500 flex items-center pointer-events-none bg-white z-20 border-b border-r border-gray-100">
+                          <span class="mr-2 h-[12px] w-[12px] opacity-80">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-music"><path d="M9 18V5l12-2v13"></path><circle cx="6" cy="18" r="3"></circle><circle cx="18" cy="16" r="3"></circle></svg>
+                          </span>
+                          Output Audio
+                      </div>
+                      <div class="h-full min-h-[8rem] flex justify-center items-center">
+                          <div class="h-5 opacity-50">
+                              <svg v-if="audio == null" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-music"><path d="M9 18V5l12-2v13"></path><circle cx="6" cy="18" r="3"></circle><circle cx="18" cy="16" r="3"></circle></svg>
+                              <audio v-if="audio != null" class="w-full" controls="" preload="metadata" :src="audio"></audio>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
     </div>
 </div>
 </template>
